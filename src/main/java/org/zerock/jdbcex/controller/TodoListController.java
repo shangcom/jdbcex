030a63c2008d673349a3493a89dc45010354971f
shangcom <dndbs91@gmail.com>
2024-05-02 10:40:28 +0900

    p153 진행중. list.jsp에 태그라이브러리 작성중. spring, springboot 버전에 따른 태그라이브러리 변경사항 정리하기.

--- src/main/java/org/zerock/jdbcex/controller/TodoListController.java
+++ src/main/java/org/zerock/jdbcex/controller/TodoListController.java
@@ -1,9 +1,16 @@
 package org.zerock.jdbcex.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.service.TodoService;
+
+import java.io.IOException;
+import java.util.List;
 
 
 @WebServlet(name = "todoListController", urlPatterns = "/todo/list")
@@ -11,4 +18,18 @@ import org.zerock.jdbcex.service.TodoService;
 public class TodoListController extends HttpServlet {
     private TodoService todoService = TodoService.INSTANCE;
 
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        log.info("todo list...........................");
+        try {
+            List<TodoDTO> dtoList = todoService.listAll();
+            req.setAttribute("dtoList", dtoList);
+            req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
